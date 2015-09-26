00c6e4f8dee3acff5e041ca2a3fff4064dd9cfe4
Charlie Clark <clarkcharlie03@gmail.com>
2015-09-26 14:40:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var gen = require('random-seed');
+var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function predicatableHash( seed, length ){
+	length = length || 10;
+    var text = "";
+    var rand = gen.create(seed);
+
+    for( var i=0; i < length; i++ )
+        text += possible.charAt(Math.floor(rand.random(1) * possible.length));
+
+    return text;
+}
+
+module.exports = predicatableHash;
